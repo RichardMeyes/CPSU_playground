b0cb35556448f14e0afd2c97f06c3e6abcf7656d
RichardMeyes <richard.meyes@ima.rwth-aachen.de>
2018-03-24 14:48:38 +0100

    added code for numerical solution to the cpsu ode and for the boxplot

--- code/plot_freq_development_boxplot.py
+++ code/plot_freq_development_boxplot.py
@@ -13,3 +13,8 @@ if __name__ == "__main__":
     with h5py.File("../data/preprocessed/CartPoleData_full.h5", 'r') as f:
         cp_data = f["CartPoleData_full"][...]
         rewards = f["EpisodeRewards"][...]
+
+
+        # ToDo: high-pass, so that low frequs are cut - then boxplot, 1900 episodes, 19 boxplots, 100 episodes per box
+        # ToDo: FFT after initial swing up, plot development of amp and freq depending on episode and on reward.
+        # ToDo: Show that Agent learns to find the correct mps and freqs as learning progresses
