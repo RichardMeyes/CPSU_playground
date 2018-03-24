1bea30d988d4bf7d78d08667586137336a2443e1
RichardMeyes <richard.meyes@ima.rwth-aachen.de>
2018-03-24 11:06:40 +0100

    renamed files for clarification and added file for plotting the frequency develpoment

---
+++ code/plot_freq_development_boxplot.py
@@ -0,0 +1,15 @@
+import h5py
+import numpy as np
+from scipy.fftpack import fft
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+    # mapping
+    idx_map = {"cx": 0, "cvx": 1, "px": 2, "py": 3, "pav": 4}
+
+    # read data
+    with h5py.File("../data/preprocessed/CartPoleData_full.h5", 'r') as f:
+        cp_data = f["CartPoleData_full"][...]
+        rewards = f["EpisodeRewards"][...]
