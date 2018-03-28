61c2d025efb215d00685c4b49da548d436de6155
Richard Meyes <richard.meyes@ima.rwth-aachen.de>
2018-03-28 16:13:39 +0200

    added more visual information to the fft average plot

--- code/plot_fft_averages.py
+++ code/plot_fft_averages.py
@@ -2,6 +2,7 @@ import h5py
 import numpy as np
 from scipy.fftpack import fft
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 from scipy.signal import butter, filtfilt
 
 
@@ -91,7 +92,7 @@ if __name__ == "__main__":
     cx_fft_episodes = np.zeros((num_episodes, N_fft // 2 - 1))
     for i_episode in range(num_episodes):
         # perform FFT on filtered movement data to detect high frequency component
-        cx_filt = butter_bandpass_filter(cx_episodes[i_episode], lowcut=2.0, highcut=5.0, fs=fs, order=5)
+        cx_filt = butter_bandpass_filter(cx_episodes[i_episode], lowcut=2.0, highcut=7.5, fs=fs, order=5)
         cx_filt_fft = fft(cx_filt[s12:])
         cx_fft_episodes[i_episode] = 2 / N_fft * np.abs(cx_filt_fft[1:N_fft // 2])
 
@@ -103,28 +104,82 @@ if __name__ == "__main__":
     #     ax.set_title('episode_ID: {0}, maximum reward: {1}'.format(i_episode + 1, rewards[i_episode]))
     #     plt.show()
 
+    # ToDo: plot Amplitude development!
+    # ToDo: check develpment of fraction of the 3.4 Hz in the power spectral density. (integral around 3.4Hz, compare to integral across all frequencies)
+    # ToDo: plot development of average y-pos
+
+    # calculate indexes for frequency band around 3.4 Hz
+    freq1 = 3.4  # Hz
+    margin = 0.1  # Hz
+    low_freq1 = freq1 - margin
+    high_freq1 = freq1 + margin
+    idx_low1 = int(len(x_cx_fft)*low_freq1/x_cx_fft[-1]) - 1  # -1, becasue the first data point of x_cx_fft is cut for plotting!
+    idx_high1 = int(len(x_cx_fft) * high_freq1 / x_cx_fft[-1]) - 1  # -1, becasue the first data point of x_cx_fft is cut for plotting!
+    peak_idx1 = int(len(x_cx_fft)*(low_freq1+high_freq1)/2/x_cx_fft[-1]) - 1
+
+    # calculate indexes for frequency band around 2.3 Hz
+    freq2 = 2.3  # Hz
+    margin = 0.2  # Hz
+    low_freq2 = freq2 - margin
+    high_freq2 = freq2 + margin
+    idx_low2 = int(len(x_cx_fft) * low_freq2 / x_cx_fft[-1]) - 1  # -1, becasue the first data point of x_cx_fft is cut for plotting!
+    idx_high2 = int(len(x_cx_fft) * high_freq2 / x_cx_fft[-1]) - 1  # -1, becasue the first data point of x_cx_fft is cut for plotting!
+    peak_idx2 = int(len(x_cx_fft) * (low_freq2 + high_freq2) / 2 / x_cx_fft[-1]) - 1
+
+
     # reorder
     cx_fft_episodes = cx_fft_episodes[episode_order]
+
+    # store avr_fft amps for 3.4 Hz peak
+    amps_high = []
+    amps_low = []
 
     fig = plt.figure(figsize=(18, 12))
     fig.subplots_adjust(top=0.95, bottom=0.1, left=0.12, right=0.95, wspace=0.6)
     for i in range(19):
         avr_fft = np.mean(cx_fft_episodes[i*100:(i+1)*100], axis=0)
+        avr_fft_std = np.std(cx_fft_episodes[i * 100:(i + 1) * 100], axis=0)
         ax = fig.add_subplot(4,5,i+1)
         ax.plot(x_cx_fft[1:], avr_fft, lw=2, c='k')
+        ax.fill_between(x_cx_fft[1:], avr_fft - avr_fft_std, avr_fft + avr_fft_std, color='k', alpha=0.3)
         ax.set_title('eIDs: {0}-{1}, av_rev {2:.2f}'.format(i*100, (i+1)*100, np.mean(rewards[episode_order][i*100:(i+1)*100])))
-        ax.axvline(x=3.4, c='k', ls='--')
+        ax.axvline(x=freq1, ymin=0, ymax=avr_fft[peak_idx1]/0.006, c='b', ls='--')
+        ax.axvline(x=freq2, ymin=0, ymax=avr_fft[peak_idx2]/0.006, c='orange', ls='--')
+        ax.axhline(y=avr_fft[peak_idx1], xmin=0, xmax=freq1/x_cx_fft[-1], c='b', ls='--')
+        ax.axhline(y=avr_fft[peak_idx2], xmin=0, xmax=freq2 / x_cx_fft[-1], c='orange', ls='--')
+        ax.set_ylim(0, 0.006)
+        amps_high.append(np.mean(avr_fft[idx_low1:idx_high1+1]))
+        amps_low.append(np.mean(avr_fft[idx_low2:idx_high2 + 1]))
+
+    def func_lin(x, a, b):
+        return a*x + b
+    popt1, pcov1 = curve_fit(func_lin, np.arange(len(amps_high))[3:], amps_high[3:])
+    popt2, pcov2 = curve_fit(func_lin, np.arange(len(amps_low))[3:], amps_low[3:])
+
+    fig = plt.figure(figsize=(6, 6))
+    fig.subplots_adjust(top=0.95, bottom=0.1, left=0.14, right=0.95, wspace=0.6)
+    ax = fig.add_subplot(111)
+    ax.plot(amps_high, c='b', marker='o', lw=2)
+    ax.plot(np.arange(len(amps_high))[3:], func_lin(np.arange(len(amps_high))[3:], *popt1), lw=2, ls='--', c='b')
+    ax.plot(amps_low, c='orange', marker='o', lw=2)
+    ax.plot(np.arange(len(amps_low))[3:], func_lin(np.arange(len(amps_low))[3:], *popt2), lw=2, ls='--', c='orange')
+    ax.set_xlabel("episode window")
+    ax.set_ylabel("amplitude")
     plt.show()
 
     fig = plt.figure(figsize=(18, 12))
     fig.subplots_adjust(top=0.95, bottom=0.1, left=0.12, right=0.95, wspace=0.6)
     for i in range(20):
         avr_fft = np.mean(cx_fft_episodes[-400:][i*20:(i+1)*20], axis=0)
+        avr_fft_std = np.std(cx_fft_episodes[-400:][i * 20:(i + 1) * 20], axis=0)
         ax = fig.add_subplot(4,5,i+1)
         ax.plot(x_cx_fft[1:], avr_fft, lw=2, c='k')
+        ax.fill_between(x_cx_fft[1:], avr_fft - avr_fft_std, avr_fft + avr_fft_std, color='k', alpha=0.3)
         ax.set_title('eIDs: {0}-{1}, av_rev {2:.2f}'.format(1500+i*20, 1500+(i+1)*20, np.mean(rewards[episode_order][-400:][i*20:(i+1)*20])))
-        ax.axvline(x=3.4, c='k', ls='--')
-    plt.show()
+        ax.axvline(x=3.4, c='b', ls='--')
+        ax.axhline(y=avr_fft[peak_idx1], xmin=0, xmax=3.4/x_cx_fft[-1], c='r', ls='-')
+        ax.set_ylim(0, 0.006)
+    # plt.show()
 
     # Interpretation and Story:
     # agents learns to develop 3.4 Hz peak with increasing reward!
@@ -133,5 +188,4 @@ if __name__ == "__main__":
     # check whether refined reward function accelerates learning
     # suggest that agent can learn to develop his own reward
     # questionable is its ability to recognize the 3.4 Hz peak as a feature to be incorporated into his own reward+
-    # neues ToDo
 
