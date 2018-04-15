709aeba0cd25d28d274307821ed22257d46226fc
RichardMeyes <richard.meyes@ima.rwth-aachen.de>
2018-04-15 12:30:31 +0200

    refined reward function. Added gaussian function and combined function. The combined function is a combination of quadratic logarithmic curvature at the edges in order to avoid the shar fall-off of the gaussian and the gaussian around the target frequency in order to avoid the sharp curvature of the quadratic logarithmic which would give too big of a reward difference for very small changes in frequency

--- code/plot_frequency_reward_component.py
+++ code/plot_frequency_reward_component.py
@@ -2,24 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def freq_rew(freqs_, f_target_, c1, c2, c3):
+def freq_rew(freqs_, f_target_, function):
     """
 
     :param freqs_: array containing the frequencies for which the reward should be evaluated
 
     :param f_target_: target frequency, which gives the highest reward
 
-    :param c1: constant that essentially determines the maximum reward at the peak frequency.
-    Chosen so that reward_max = 1000
-
-    :param c2: constant that determines the curvature of the reward function, i.e. the fall-off with growing distance
-    from the target frequency. Smaller values  lead to a flatter fall-off.
-
-    :param c3: constant that determines the reward-offset. Changes the maximum reward too.
+    :param function_: shape of the reward function. Can either be 'log', 'gaussian' or 'combined'
 
     :return: array containing the calculated reward for the given frequency array
     """
-    return c1 * -np.log(c2 * (freqs_ - f_target_) ** 2) + c3
+
+    if function == 'log':
+        c1 = 81.41
+        c2 = 0.2
+        c3 = 0
+        return c1 * -np.log(c2 * (freqs_ - f_target_) ** 2) + c3
+    elif function == 'gaussian':
+        c1 = 100
+        c2 = 0.5
+        c3 = 1100
+        return c1 * -np.exp(c2 * (freqs_ - f_target_) ** 2) + c3
+    elif function == 'combined':
+        c1 = 500
+        c2 = 1.0
+        c3 = 1500
+        gaussian = c1 * -np.exp(c2 * (freqs_ - f_target_) ** 2) + c3
+        zp1 = np.where(gaussian > 0)[0][0]
+        zp2 = len(gaussian) - zp1  # symmetric function!
+
+        c1 = 81.41
+        c2 = 0.9
+        c3 = 0
+        log = c1 * -np.log(c2 * (freqs_ - f_target_) ** 2) + c3
+
+        combined = np.zeros(len(freqs_))
+        combined[:zp1] = log[:zp1]
+        combined[zp1:zp2] = gaussian[zp1:zp2]
+        combined[zp2:] = log[zp2:]
+
+        return combined
 
 
 if __name__ == "__main__":
@@ -27,7 +50,8 @@ if __name__ == "__main__":
     f_target = 3.4  # Hz
     margin = 5  # Hz
     freqs = np.linspace(f_target - margin, f_target + margin, 1000, endpoint=True)
-    reward = freq_rew(freqs_=freqs, f_target_=f_target, c1=81.94, c2=0.2, c3=0)
+    rew_function = 'combined'
+    reward = freq_rew(freqs_=freqs, f_target_=f_target, function=rew_function)
     print("maximum reward:", np.max(reward))
 
     zero_point1 = np.where(reward > 0)[0][0]
@@ -44,6 +68,7 @@ if __name__ == "__main__":
     ax.axvline(x=f_target, lw=3, ls='--', c='orange', label="target frequency: {0} Hz".format(f_target))
     ax.axvline(x=0, lw=2, ls='-', c='k')
     ax.axhline(y=0, lw=2, ls='-', c='k')
+    ax.set_ylim(-1100, 1100)
     ax.set_ylabel("reward")
     ax.set_xlabel("frequency [Hz]")
     ax.grid()
